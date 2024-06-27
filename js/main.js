998d6e1dd42d247fbcf864ff6c0a6016130a66fe
battistaennio <ennio.battista99@gmail.com>
2024-06-27 15:23:25 +0200

    build: add base project files

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+//https://flynn.boolean.careers/exercises/api/random/mail
+
+const { createApp } = Vue;
+
+createApp({
+
+    data(){
+        return {
+            email: "",
+
+
+        }
+    },
+
+    methods: {
+
+
+
+    },
+
+    mounted() {
+
+
+
+    }
+
+}).mount("#app")
